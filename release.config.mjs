ce5bba3cfb67095d28c6db275d764b1832be2ebd
wadefletch <wbfletch@gmail.com>
2025-08-05 18:03:18 -0500

    chore: follow semantic-release best-practices

--- release.config.mjs
+++ release.config.mjs
@@ -10,17 +10,8 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
     "semantic-release-commits-lint",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ],
 };
